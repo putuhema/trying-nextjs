a11dc19f0655ce07d8bfb5cfddf6770d138ae2ca
putu mahendra <putuhendramahendra@gmail.com>
2023-11-06 07:44:19 +0800

    finish the course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,30 @@
-const Page = () => {
-  return <div>Customer Page</div>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+import { Suspense } from "react";
+export const metadata: Metadata = {
+  title: "Customer",
+};
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div>
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Page;
--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>dashboard page</div>;
-};
-
-export default Page;
